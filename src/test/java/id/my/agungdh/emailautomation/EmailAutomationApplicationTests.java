7ba407e832cda22af4a2697d71485969806af63f
AgungDH <agungdh@live.com>
2025-11-03 11:27:20 +0700

    init

---
+++ src/test/java/id/my/agungdh/emailautomation/EmailAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.emailautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
